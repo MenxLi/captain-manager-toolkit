ce20770e43b0d7583c1378c003b4c250d280fcab
lab-a6000 <limengxun45@outlook.com>
2025-02-08 19:41:48 +0800

    use user cursor

--- pody/eng/user.py
+++ pody/eng/user.py
@@ -1,16 +1,10 @@
 import sqlite3
 import hashlib
 import dataclasses
-from threading import Lock
+from contextlib import contextmanager
 
 from .errors import InvalidUsernameError
 from ..config import DATA_HOME
-
-def locked(func):
-    def wrapper(*args, **kwargs):
-        with args[0].lock:
-            return func(*args, **kwargs)
-    return wrapper
 
 def hash_password(username: str, password: str):
     return hashlib.sha256(f"{username}:{password}".encode()).hexdigest()
@@ -31,6 +25,7 @@ def check_username(username: str):
 class User:
     userid: int
     name: str
+    is_admin: bool
     max_pods: int
 
 class UserDatabase:
@@ -38,65 +33,86 @@ class UserDatabase:
 
         DATA_HOME.mkdir(exist_ok=True)
         self.conn = sqlite3.connect(DATA_HOME / "users.db", check_same_thread=False)
-        self.cursor = self.conn.cursor()
-        self.lock = Lock()
 
-        with self.lock:
-            self.cursor.execute(
+        with self.transaction() as cursor:
+            cursor.execute(
                 """
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY,
                     username TEXT NOT NULL,
                     credential TEXT NOT NULL
+                    is_admin BOOLEAN NOT NULL DEFAULT 0,
                     max_pods INTEGER NOT NULL DEFAULT 1
                 )
                 """
             )
-            self.conn.commit()
-
-    @locked
-    def add_user(self, username: str, password: str, max_pods: int = 1):
-        check_username(username)
-        self.cursor.execute(
-            "INSERT INTO users (username, credential, max_pods) VALUES (?, ?, ?)",
-            (username, hash_password(username, password), max_pods),
-        )
-        self.conn.commit()
-
-    @locked
-    def update_password(self, username: str, password: str):
-        check_username(username)
-        self.cursor.execute(
-            "UPDATE users SET credential = ? WHERE username = ?",
-            (hash_password(username, password), username),
-        )
-        self.conn.commit()
     
-    @locked
-    def update_max_pods(self, username: str, max_pods: int):
-        self.cursor.execute(
-            "UPDATE users SET max_pods = ? WHERE username = ?",
-            (max_pods, username),
-        )
-        self.conn.commit()
+    def cursor(self):
+        @contextmanager
+        def _cursor():
+            cursor = self.conn.cursor()
+            try:
+                yield cursor
+            finally:
+                cursor.close()
+        return _cursor()
+    
+    def transaction(self):
+        @contextmanager
+        def _transaction():
+            cursor = self.conn.cursor()
+            try:
+                yield cursor
+            except Exception as e:
+                cursor.execute("ROLLBACK")
+                raise e
+            else:
+                self.conn.commit()
+            finally:
+                cursor.close()
+        return _transaction()
 
-    @locked
+    def add_user(self, username: str, password: str, is_admin: bool = False, max_pods: int = 1):
+        check_username(username)
+        with self.transaction() as cursor:
+            cursor.execute(
+                "INSERT INTO users (username, credential, is_admin, max_pods) VALUES (?, ?, ?, ?)",
+                (username, hash_password(username, password), is_admin, max_pods),
+            )
+    
+    def update_user(self, username: str, **kwargs):
+        check_username(username)
+        if 'password' in kwargs:
+            with self.transaction() as c:
+                c.execute("UPDATE users SET credential = ? WHERE username = ?", (hash_password(username, kwargs.pop('password')), username))
+                print("Password updated")
+        if 'max_pods' in kwargs:
+            with self.transaction() as c:
+                c.execute("UPDATE users SET max_pods = ? WHERE username = ?", (kwargs.pop('max_pods'), username))
+                print("Max pods updated")
+        if 'is_admin' in kwargs:
+            with self.transaction() as c:
+                c.execute("UPDATE users SET is_admin = ? WHERE username = ?", (kwargs.pop('is_admin'), username))
+                print("Admin status updated")
+    
+    def has_user(self, username: str)->bool:
+        with self.cursor() as cur:
+            cur.execute("SELECT id FROM users WHERE username = ?", (username,))
+            return cur.fetchone() is not None
+
     def check_user(self, credential: str):
-        cur = self.conn.execute(
-            "SELECT id, username, max_pods FROM users WHERE credential = ?",
-            (credential,),
-        )
-        res = cur.fetchone()
-        if res is None: return User(0, '', 0)
-        else: return User(*res)
+        with self.cursor() as cur:
+            cur.execute("SELECT id FROM users WHERE credential = ?", (credential,))
+            res = cur.fetchone()
+            if res is None: return User(0, '', False, 0)
+            else: return User(*res)
     
-    @locked
     def delete_user(self, username: str):
-        self.cursor.execute(
-            "DELETE FROM users WHERE username = ?",
-            (username,),
-        )
-        self.conn.commit()
+        with self.transaction() as cursor:
+            cursor.execute(
+                "DELETE FROM users WHERE username = ?",
+                (username,),
+            )
 
     def close(self):
         self.conn.close()
