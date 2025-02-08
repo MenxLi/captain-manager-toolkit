a857c98b8a194c98c0648f7bf498d07f9c51175c
lab-a6000 <limengxun45@outlook.com>
2025-02-08 19:51:34 +0800

    user cli

--- pody/eng/user.py
+++ pody/eng/user.py
@@ -39,8 +39,8 @@ class UserDatabase:
                 """
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY,
-                    username TEXT NOT NULL,
-                    credential TEXT NOT NULL
+                    username TEXT NOT NULL UNIQUE,
+                    credential TEXT NOT NULL, 
                     is_admin BOOLEAN NOT NULL DEFAULT 0,
                     max_pods INTEGER NOT NULL DEFAULT 1
                 )
@@ -62,6 +62,7 @@ class UserDatabase:
         def _transaction():
             cursor = self.conn.cursor()
             try:
+                cursor.execute("BEGIN")
                 yield cursor
             except Exception as e:
                 cursor.execute("ROLLBACK")
@@ -79,18 +80,21 @@ class UserDatabase:
                 "INSERT INTO users (username, credential, is_admin, max_pods) VALUES (?, ?, ?, ?)",
                 (username, hash_password(username, password), is_admin, max_pods),
             )
+            res = cursor.lastrowid
+            print(f"User {username} added with id {res}")
     
     def update_user(self, username: str, **kwargs):
+        print(f"Updating user {username} with {kwargs}")
         check_username(username)
-        if 'password' in kwargs:
+        if 'password' in kwargs and kwargs['password'] is not None:
             with self.transaction() as c:
                 c.execute("UPDATE users SET credential = ? WHERE username = ?", (hash_password(username, kwargs.pop('password')), username))
                 print("Password updated")
-        if 'max_pods' in kwargs:
+        if 'max_pods' in kwargs and kwargs['max_pods'] is not None:
             with self.transaction() as c:
                 c.execute("UPDATE users SET max_pods = ? WHERE username = ?", (kwargs.pop('max_pods'), username))
                 print("Max pods updated")
-        if 'is_admin' in kwargs:
+        if 'is_admin' in kwargs and kwargs['is_admin'] is not None:
             with self.transaction() as c:
                 c.execute("UPDATE users SET is_admin = ? WHERE username = ?", (kwargs.pop('is_admin'), username))
                 print("Admin status updated")
